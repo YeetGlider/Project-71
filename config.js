3b03d615e81a9ff9f182aa5db2fbae5097080edd
YeetGlider <72340148+YeetGlider@users.noreply.github.com>
2022-02-05 12:50:50 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase'
+require("@firebase/firestore")
+
+const firebaseConfig = {
+ //Add configuration here
+ const :firebaseConfig = {
+  apiKey: "AIzaSyBCpj4Ifl0vP5PL1kNhurWsvZR4obPikco",
+  authDomain: "complaint-forum-1e544.firebaseapp.com",
+  projectId: "complaint-forum-1e544",
+  storageBucket: "complaint-forum-1e544.appspot.com",
+  messagingSenderId: "898795206050",
+  appId: "1:898795206050:web:1c89b5f288ad0c028e6efb"
+},
+ 
+};
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase.firestore()
+
